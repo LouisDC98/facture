06c51140b178c2126ad870e33b5fe1dd39ea2e78
LouisDC98 <louisdacosta98@gmail.com>
2024-05-12 02:25:41 +0200

    fix: fix result for BilanTicket

--- src/component/BilanTicket/BilanTicket.jsx
+++ src/component/BilanTicket/BilanTicket.jsx
@@ -8,18 +8,18 @@ function BilanTicket(props) {
         <section className='bilanTicket'>
             <div className='ticketGridBilan'>
                 <p className='ticketAvRemise'>Total avant remises</p>
-                <p className='ticketAvRemise secondColumn'>{totaux.totalPanier - 0.7}€</p>
+                <p className='ticketAvRemise secondColumn'>{(totaux.totalPanier - 0.7).toFixed(2)}€</p>
 
                 <p>Total Remises immédiates remises</p>
                 <p className='secondColumn colorRemise'>-{totaux.totalRemises}€</p>
 
                 <p className='ticketTotalPrix'>Total à payer</p>
-                <p className='ticketTotalPrix secondColumn'>{totaux.totalPanier - 0.7 - totaux.totalRemises}€</p>
+                <p className='ticketTotalPrix secondColumn'>{(totaux.totalPanier - 0.7 - totaux.totalRemises).toFixed(2)}€</p>
 
                 <p className='ticketAvRemise'>Payé par</p>
                 <div></div>
                 <p>Cagnote fidélité utilisée</p>
-                <p className='secondColumn colorRemise'>-{totaux.totalPanier - 0.7 - totaux.totalRemises}€</p>
+                <p className='secondColumn colorRemise'>-{(totaux.totalPanier - 0.7 - totaux.totalRemises).toFixed(2)}€</p>
             </div>
 
             <div className='ticketGridTVA'>
@@ -28,20 +28,20 @@ function BilanTicket(props) {
                 <p className='ticketAvRemise secondColumn'>dont TVA</p>
 
                 {tvaArray.prix5 !== "0.00" && <p>5.5%</p>}
-                {tvaArray.prix5 !== "0.00" && <p className='secondColumn'>{tvaArray.prix5}€</p>}
+                {tvaArray.prix5 !== "0.00" && <p className='secondColumn'>{(Number(tvaArray.prix5) + Number(tvaArray.tva5)).toFixed(2)}€</p>}
                 {tvaArray.prix5 !== "0.00" && <p className='secondColumn'>{tvaArray.tva5}€</p>}
 
                 {tvaArray.prix10 !== "0.00" && <p>10.0%</p>}
-                {tvaArray.prix10 !== "0.00" && <p className='secondColumn'>{tvaArray.prix10}€</p>}
+                {tvaArray.prix10 !== "0.00" && <p className='secondColumn'>{(Number(tvaArray.prix10) + Number(tvaArray.tva10)).toFixed(2)}€</p>}
                 {tvaArray.prix10 !== "0.00" && <p className='secondColumn'>{tvaArray.tva10}€</p>}
 
                 {tvaArray.prix20 !== "0.00" && <p>20.0%</p>}
-                {tvaArray.prix20 !== "0.00" && <p className='secondColumn'>{tvaArray.prix20}€</p>}
+                {tvaArray.prix20 !== "0.00" && <p className='secondColumn'>{(Number(tvaArray.prix20) + Number(tvaArray.tva20)).toFixed(2)}€</p>}
                 {tvaArray.prix20 !== "0.00" && <p className='secondColumn'>{tvaArray.tva20}€</p>}
 
 
                 <p className='ticketAvRemise dashedBorder'>Total TVA</p>
-                <p className='ticketAvRemise secondColumn dashedBorder'>{tvaArray.totalPrix-0.7}€</p>
+                <p className='ticketAvRemise secondColumn dashedBorder'>{(Number(tvaArray.totalPrix) - 0.7 + Number(tvaArray.totalTVA)).toFixed(2)}€</p>
                 <p className='ticketAvRemise secondColumn dashedBorder'>{tvaArray.totalTVA}€</p>
             </div>
 
