efd082ae2a48b364801b09e89e38ac30bc514798
LouisDC <louisdacosta98@gmail.com>
2025-07-23 14:19:43 +0200

    Style: ManageProfile css

--- src/component/Modals/ManageProfile/ManageProfile.jsx
+++ src/component/Modals/ManageProfile/ManageProfile.jsx
@@ -64,116 +64,108 @@ function ManageProfile(props) {
     };
 
     const onSubmit = (data) => {
-        console.log('data', data)
         data.newItem.country = "France"
         action(data.newItem)
     };
 
     return (
-        <div className='displayModal'>
+        <div className='displayModal manageProfile'>
             <div className='modalBg'>
                 <button onClick={() => closeModal()} className='buttonIcon closeButton' />
-                <div className='confirmModal'>
-                    <form onSubmit={handleSubmit(onSubmit)} className='form profilForm'>
+                <form onSubmit={handleSubmit(onSubmit)} className='form'>
 
-                        <div className='inputProfile'>
-                            <div className='titleInput'>Prénom</div>
-                            <input className='field' type='text' {...register(`newItem.firstName`, { required: true })}></input>
+                    <div className='input'>
+                        <div className='titleInput'>Prénom</div>
+                        <input className='field' type='text' {...register(`newItem.firstName`, { required: true })}></input>
+                    </div>
+
+                    <div className='input'>
+                        <div className='titleInput'>Nom</div>
+                        <input className='field' type='text' {...register(`newItem.lastName`, { required: true })}></input>
+                    </div>
+                    <div className='input genderCol'>
+                        <div className='radioGroup'>
+                            <label>
+                                <input
+                                    type='radio'
+                                    value='male'
+                                    {...register('newItem.gender', { required: true })}
+                                />
+                                Homme
+                            </label>
+                            <label>
+                                <input
+                                    type='radio'
+                                    value='female'
+                                    {...register('newItem.gender', { required: true })}
+                                />
+                                Femme
+                            </label>
                         </div>
-
-                        <div className='inputProfile'>
-                            <div className='titleInput'>Nom</div>
-                            <input className='field' type='text' {...register(`newItem.lastName`, { required: true })}></input>
-                        </div>
-                        <div className='inputProfile genderCol'>
-                            <div className='radioGroup'>
-                                <label>
-                                    <input
-                                        type='radio'
-                                        value='male'
-                                        {...register('newItem.gender', { required: true })}
-                                    />
-                                    Homme
-                                </label>
-                                <label>
-                                    <input
-                                        type='radio'
-                                        value='female'
-                                        {...register('newItem.gender', { required: true })}
-                                    />
-                                    Femme
-                                </label>
-                            </div>
-                        </div>
+                    </div>
 
 
-                        <div className='inputProfile adressCol'>
-                            <div className='titleInput'>Adresse</div>
-                            <input className='field' type='text' {...register(`newItem.adresse`, { required: true })}></input>
-                        </div>
+                    <div className='input adressCol'>
+                        <div className='titleInput'>Adresse</div>
+                        <input className='field' type='text' {...register(`newItem.adresse`, { required: true })}></input>
+                    </div>
 
-                        <div className='inputProfile'>
-                            <div className='titleInput'>Ville</div>
-                            <input className='field' type='text' {...register(`newItem.city`, { required: true })}></input>
-                        </div>
+                    <div className='input'>
+                        <div className='titleInput'>Ville</div>
+                        <input className='field' type='text' {...register(`newItem.city`, { required: true })}></input>
+                    </div>
 
-                        <div className='inputProfile'>
-                            <div className='titleInput'>Code Postal</div>
-                            <input className='field' type='text' {...register(`newItem.codePostal`, { required: true })}></input>
-                        </div>
+                    <div className='input'>
+                        <div className='titleInput'>Code Postal</div>
+                        <input className='field' type='text' {...register(`newItem.codePostal`, { required: true })}></input>
+                    </div>
 
-                        {/* <div className='inputProfile'>
+                    {/* <div className='input'>
                             <div className='titleInput'>Pays</div>
                             <input className='field' type='text' disabled placeholder='France' {...register(`newItem.country`)}></input>
                         </div> */}
 
-                        <div className='inputProfile'>
-                            <div className='titleInput'>Numéro de carte</div>
-                            <input className='field' type='number' min={0} step="any" disabled={cardNumberRandom} {...register(`newItem.cardNumber`, { required: true, valueAsNumber: true })}></input>
-                        </div>
-                        <div className='inputProfile'>
-                            <div className='titleInput'>Numéro client</div>
-                            <input className='field' type='number' min={0} step="any" disabled={clientIDRandom} {...register(`newItem.clientID`, { required: true, valueAsNumber: true })}></input>
-                        </div>
-                        <div className='randomDiv'>
-                            <input type='checkbox' checked={cardNumberRandom} onChange={(e) => setCardNumberRandom(e.target.checked)} />
-                            <label>Random numéro de carte</label>
-                        </div>
+                    <div className='input'>
+                        <div className='titleInput'>Numéro de carte</div>
+                        <input className='field' type='number' min={0} step="any" disabled={cardNumberRandom} {...register(`newItem.cardNumber`, { required: true, valueAsNumber: true })}></input>
+                    </div>
+                    <div className='input'>
+                        <div className='titleInput'>Numéro client</div>
+                        <input className='field' type='number' min={0} step="any" disabled={clientIDRandom} {...register(`newItem.clientID`, { required: true, valueAsNumber: true })}></input>
+                    </div>
+                    <div className='randomDiv'>
+                        <input type='checkbox' checked={cardNumberRandom} onChange={(e) => setCardNumberRandom(e.target.checked)} />
+                        <label>Random numéro de carte</label>
+                    </div>
 
-                        <div className='randomDiv'>
-                            <input type='checkbox' checked={clientIDRandom} onChange={(e) => setClientIDRandom(e.target.checked)} />
-                            <label>Random numéro de client</label>
-                        </div>
+                    <div className='randomDiv'>
+                        <input type='checkbox' checked={clientIDRandom} onChange={(e) => setClientIDRandom(e.target.checked)} />
+                        <label>Random numéro de client</label>
+                    </div>
 
-                        <div className='inputProfile'>
-                            <div className='titleInput'>Propriétaire</div>
-                            <select className='field selectTVA' {...register(`newItem.ownerID`, { required: true })}>
-                                <option value="1">Louis</option>
-                                <option value="2">Yohan</option>
-                            </select>
-                        </div>
+                    <div className='input'>
+                        <div className='titleInput'>Propriétaire</div>
+                        <select className='field select' {...register(`newItem.ownerID`, { required: true })}>
+                            <option value="1">Louis</option>
+                            <option value="2">Yohan</option>
+                        </select>
+                    </div>
 
-                        {/* <div className='inputProfile'>
-                            <div className='titleInput'>Magasin</div>
-                            <input className='field' type='number' min={0} step="any" {...register(`newItem.magasinID`, { required: true, valueAsNumber: true })}></input>
-                        </div> */}
-
-                        <div className='inputProfile'>
-                            <div className='titleInput'>Magasin</div>
-                            <select className='field selectTVA' {...register(`newItem.magasinID`, { required: true })}>
-                                <option value="">-- Sélectionnez un magasin --</option>
-                                {storeList?.map((store) => (
-                                    <option key={store.id} value={store.id}>
-                                        {store.name}
-                                    </option>
-                                ))}
-                            </select>
-                        </div>
+                    <div className='input'>
+                        <div className='titleInput'>Magasin</div>
+                        <select className='field select' {...register(`newItem.magasinID`, { required: true })}>
+                            <option value="">-- Sélectionnez un magasin --</option>
+                            {storeList?.map((store) => (
+                                <option key={store.id} value={store.id}>
+                                    {store.name}
+                                </option>
+                            ))}
+                        </select>
+                    </div>
 
 
-                        <button type='submit' className='submitEdit submitCol'>{selectedItem ? "Confirmer les modifications" : "Créer l'article"}</button>
-                    </form>
-                </div>
+                    <button type='submit' className='submitBtn submitCol'>{selectedItem ? "Confirmer les modifications" : "Créer l'article"}</button>
+                </form>
             </div>
         </div>
     )
