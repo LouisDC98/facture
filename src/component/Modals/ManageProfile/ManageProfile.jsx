554ee9aa8254b41e9db034c73da0790fa4399718
LouisDC <louisdacosta98@gmail.com>
2025-07-24 11:58:22 +0200

    Style: manageProfile css review

--- src/component/Modals/ManageProfile/ManageProfile.jsx
+++ src/component/Modals/ManageProfile/ManageProfile.jsx
@@ -9,40 +9,38 @@ function ManageProfile(props) {
     const [cardNumberRandom, setCardNumberRandom] = useState(true)
     const [clientIDRandom, setClientIDRandom] = useState(true)
 
-    const { register, handleSubmit, control, setValue } = useForm({
+    const { register, handleSubmit, setValue } = useForm({
         defaultValues: {
-            newItem: {
-                profileID: selectedItem?.profileID,
-                firstName: selectedItem?.firstName,
-                lastName: selectedItem?.lastName,
-                adresse: selectedItem?.adresse,
-                city: selectedItem?.city,
-                codePostal: selectedItem?.codePostal,
-                country: selectedItem?.country,
-                cardNumber: selectedItem?.cardNumber,
-                clientID: selectedItem?.clientID,
-                magasinID: selectedItem?.magasinID,
-                ownerID: selectedItem?.ownerID,
-                gender: selectedItem?.gender,
-            }
+            profileID: selectedItem?.profileID,
+            firstName: selectedItem?.firstName,
+            lastName: selectedItem?.lastName,
+            adresse: selectedItem?.adresse,
+            city: selectedItem?.city,
+            codePostal: selectedItem?.codePostal,
+            country: selectedItem?.country,
+            cardNumber: selectedItem?.cardNumber,
+            clientID: selectedItem?.clientID,
+            magasinID: selectedItem?.magasinID,
+            ownerID: selectedItem?.ownerID,
+            gender: selectedItem?.gender,
         }
     });
 
     useEffect(() => {
         if (cardNumberRandom && !selectedItem) {
             const randomCardNumber = generateCardNumber();
-            setValue("newItem.cardNumber", randomCardNumber);
+            setValue("cardNumber", randomCardNumber);
         } else if (!selectedItem) {
-            setValue("newItem.cardNumber", null);
+            setValue("cardNumber", null);
         }
     }, [cardNumberRandom]);
 
     useEffect(() => {
         if (clientIDRandom && !selectedItem) {
             const randomClientID = generateClientID();
-            setValue("newItem.clientID", randomClientID);
+            setValue("clientID", randomClientID);
         } else if (!selectedItem) {
-            setValue("newItem.clientID", null);
+            setValue("clientID", null);
         }
     }, [clientIDRandom]);
 
@@ -64,32 +62,26 @@ function ManageProfile(props) {
     };
 
     const onSubmit = (data) => {
-        data.newItem.country = "France"
-        action(data.newItem)
+        data.country = "France"
+        action(data)
     };
 
     return (
         <div className='displayModal manageProfile'>
             <div className='modalBg'>
-                <button onClick={() => closeModal()} className='buttonIcon closeButton' />
+                <div className='moddalHeader'>
+                    <h3 className='titleModal'>{selectedItem ? "Modifier un profil" : "Créer un profil"}</h3>
+                    <button onClick={() => closeModal()} className='closeButtonNew' />
+                </div>
+                <span className='spanRow'></span>
                 <form onSubmit={handleSubmit(onSubmit)} className='form'>
-
-                    <div className='input'>
-                        <div className='titleInput'>Prénom</div>
-                        <input className='field' type='text' {...register(`newItem.firstName`, { required: true })}></input>
-                    </div>
-
-                    <div className='input'>
-                        <div className='titleInput'>Nom</div>
-                        <input className='field' type='text' {...register(`newItem.lastName`, { required: true })}></input>
-                    </div>
                     <div className='input genderCol'>
                         <div className='radioGroup'>
                             <label>
                                 <input
                                     type='radio'
                                     value='male'
-                                    {...register('newItem.gender', { required: true })}
+                                    {...register('gender', { required: true })}
                                 />
                                 Homme
                             </label>
@@ -97,41 +89,51 @@ function ManageProfile(props) {
                                 <input
                                     type='radio'
                                     value='female'
-                                    {...register('newItem.gender', { required: true })}
+                                    {...register('gender', { required: true })}
                                 />
                                 Femme
                             </label>
                         </div>
                     </div>
 
+                    <div className='input'>
+                        <div className='titleInput'>Prénom</div>
+                        <input className='field' type='text' {...register(`firstName`, { required: true })}></input>
+                    </div>
+
+                    <div className='input'>
+                        <div className='titleInput'>Nom</div>
+                        <input className='field' type='text' {...register(`lastName`, { required: true })}></input>
+                    </div>
+
 
                     <div className='input adressCol'>
                         <div className='titleInput'>Adresse</div>
-                        <input className='field' type='text' {...register(`newItem.adresse`, { required: true })}></input>
+                        <input className='field' type='text' {...register(`adresse`, { required: true })}></input>
                     </div>
 
                     <div className='input'>
                         <div className='titleInput'>Ville</div>
-                        <input className='field' type='text' {...register(`newItem.city`, { required: true })}></input>
+                        <input className='field' type='text' {...register(`city`, { required: true })}></input>
                     </div>
 
                     <div className='input'>
                         <div className='titleInput'>Code Postal</div>
-                        <input className='field' type='text' {...register(`newItem.codePostal`, { required: true })}></input>
+                        <input className='field' type='text' {...register(`codePostal`, { required: true })}></input>
                     </div>
 
                     {/* <div className='input'>
                             <div className='titleInput'>Pays</div>
-                            <input className='field' type='text' disabled placeholder='France' {...register(`newItem.country`)}></input>
+                            <input className='field' type='text' disabled placeholder='France' {...register(`country`)}></input>
                         </div> */}
 
                     <div className='input'>
                         <div className='titleInput'>Numéro de carte</div>
-                        <input className='field' type='number' min={0} step="any" disabled={cardNumberRandom} {...register(`newItem.cardNumber`, { required: true, valueAsNumber: true })}></input>
+                        <input className='field' type='number' min={0} step="any" disabled={cardNumberRandom} {...register(`cardNumber`, { required: true, valueAsNumber: true })}></input>
                     </div>
                     <div className='input'>
                         <div className='titleInput'>Numéro client</div>
-                        <input className='field' type='number' min={0} step="any" disabled={clientIDRandom} {...register(`newItem.clientID`, { required: true, valueAsNumber: true })}></input>
+                        <input className='field' type='number' min={0} step="any" disabled={clientIDRandom} {...register(`clientID`, { required: true, valueAsNumber: true })}></input>
                     </div>
                     <div className='randomDiv'>
                         <input type='checkbox' checked={cardNumberRandom} onChange={(e) => setCardNumberRandom(e.target.checked)} />
@@ -145,7 +147,7 @@ function ManageProfile(props) {
 
                     <div className='input'>
                         <div className='titleInput'>Propriétaire</div>
-                        <select className='field select' {...register(`newItem.ownerID`, { required: true })}>
+                        <select className='field select' {...register(`ownerID`, { required: true })}>
                             <option value="1">Louis</option>
                             <option value="2">Yohan</option>
                         </select>
@@ -153,7 +155,7 @@ function ManageProfile(props) {
 
                     <div className='input'>
                         <div className='titleInput'>Magasin</div>
-                        <select className='field select' {...register(`newItem.magasinID`, { required: true })}>
+                        <select className='field select' {...register(`magasinID`, { required: true })}>
                             <option value="">-- Sélectionnez un magasin --</option>
                             {storeList?.map((store) => (
                                 <option key={store.id} value={store.id}>
